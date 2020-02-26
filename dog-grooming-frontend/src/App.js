ffd2a6ead9c9d59be53a6f744edea2f7c5544dda
rofadalua <fafi@Fabis-MBP.attlocal.net>
2020-02-25 18:38:19 -0600

    set up reducer,fetchaction

--- dog-grooming-frontend/src/App.js
+++ dog-grooming-frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
-
+import {connect} from 'react-redux'
+import {fetchGroomer} from './actions/fetchGroomer'
 
 class App extends React.Component{
   
+  componentDidMount() {
+   this.props.fetchGroomer({type: "FETCH_GROOMER", payload: {name: 'Booking'}})
+  }
   render() {
     return (
       <div className="App">
@@ -13,5 +16,8 @@ class App extends React.Component{
   }
 }
 
+// const mapStateToProps = () => //i want to access to this particular part of the store 
 
-export default App;
+export default connect(null, {fetchGroomer})(App); 
+//is calling automatically dispatch on the return value of fetch request
+//store.dispatch({type:'FETCH....etc})-the fetch is in fetchgro file
--- dog-grooming-frontend/src/index.js
+++ dog-grooming-frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'; //change different function
-import thunk from 'react-thunk';
+import thunk from "redux-thunk";
 import { Provider }from 'react-redux'; //to access reduxstore, wrapping App
+import groomerReducer from './reducer/groomerReducer'
+
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;  //setting up the middleware 
 
-let myStore = createStore(reducer, composeEnhancers(applyMiddleware(thunk))) //reducer first, pass thunk, - thunk is what we gonna use it 
+let myStore = createStore(groomerReducer, composeEnhancers(applyMiddleware(thunk))) //reducer first, pass thunk, - thunk is what we gonna use it 
 //reducer take the action object and update what we have in our current store it returns a new version of the store
 
 ReactDOM.render(
